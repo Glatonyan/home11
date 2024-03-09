8dc219f7002988d6a8079b9adfa3f75e21c6cc2f
Ursa PC <superfraer3@gmail.com>
2024-03-09 21:23:25 +0300

    исправил замечания

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
 
         Book book1 = new Book("Война и мир", author1, 1865);
         Book book2 = new Book("Капитанская дочка", author2, 1836);
-        System.out.println("Название книги 1: " + book1.getTitle());
-        System.out.println("Автор книги 1: " + book1.getAuthor().getFirstName() + book1.getAuthor().getLastName());
-        System.out.println("Год публикации книги 1: " + book1.getPublicationYear());
-        System.out.println("Название книги 2: " + book2.getTitle());
-        System.out.println("Автор книги 2: " + book2.getAuthor().getFirstName() + book2.getAuthor().getLastName());
-        System.out.println("Год публикации книги 2: " + book2.getPublicationYear());
+        System.out.println("Название книги 1: " + book1.getTitle() + "\n");
+        System.out.println("Автор книги 1: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() + "\n");
+        System.out.println("Год публикации книги 1: " + book1.getPublicationYear() + "\n");
+        System.out.println("Название книги 2: " + book2.getTitle() + "\n");
+        System.out.println("Автор книги 2: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName() + "\n");
+        System.out.println("Год публикации книги 2: " + book2.getPublicationYear() + "\n");
 
         book1.setPublicationYear(1867);
-        System.out.println("Новый год публикации книги 1: " + book1.getPublicationYear());
+        System.out.println("Новый год публикации книги 1: " + book1.getPublicationYear() + "\n");
     }
 }
