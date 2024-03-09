74c7a2f5486fbb23d4ad3cc25e27d47e4158c573
Ursa PC <superfraer3@gmail.com>
2024-03-09 14:17:54 +0300

    домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("L.", "Tolstoy");
+        Author author2 = new Author("A.", "Pushkin");
+
+        Book book1 = new Book("Война и мир", author1, 1865);
+        Book book2 = new Book("Капитанская дочка", author2, 1836);
+        System.out.println("Название книги 1: " + book1.getTitle());
+        System.out.println("Автор книги 1: " + book1.getAuthor().getFirstName() + book1.getAuthor().getLastName());
+        System.out.println("Год публикации книги 1: " + book1.getPublicationYear());
+        System.out.println("Название книги 2: " + book2.getTitle());
+        System.out.println("Автор книги 2: " + book2.getAuthor().getFirstName() + book2.getAuthor().getLastName());
+        System.out.println("Год публикации книги 2: " + book2.getPublicationYear());
+
+        book1.setPublicationYear(1867);
+        System.out.println("Новый год публикации книги 1: " + book1.getPublicationYear());
     }
 }
