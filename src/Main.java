7eaf305e9c1ef8994079848bb736404543cf6087
Ursa PC <superfraer3@gmail.com>
2024-03-10 22:47:04 +0300

    домашка

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,13 @@ public class Main {
 
         Book book1 = new Book("Война и мир", author1, 1865);
         Book book2 = new Book("Капитанская дочка", author2, 1836);
-        System.out.println("Название книги 1: " + book1.getTitle() + "\n");
-        System.out.println("Автор книги 1: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() + "\n");
-        System.out.println("Год публикации книги 1: " + book1.getPublicationYear() + "\n");
-        System.out.println("Название книги 2: " + book2.getTitle() + "\n");
-        System.out.println("Автор книги 2: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName() + "\n");
-        System.out.println("Год публикации книги 2: " + book2.getPublicationYear() + "\n");
+
+        System.out.println(book1.toString());;
+        System.out.println(book2.toString());
 
         book1.setPublicationYear(1867);
-        System.out.println("Новый год публикации книги 1: " + book1.getPublicationYear() + "\n");
+        System.out.println("Новый год публикации книги 1:\n " + book1.getPublicationYear());
+
+
     }
 }
